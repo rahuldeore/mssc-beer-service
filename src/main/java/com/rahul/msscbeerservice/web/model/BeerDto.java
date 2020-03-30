b52ac982bd7372e5a4377e1277893f4eab236870
RahulD <rahul.deore@hotmail.com>
2020-03-29 20:40:07 -0400

    Added ControllerAdvice exception handler and added Bean validations. Closes #9 from SFG and closes #4

--- src/main/java/com/rahul/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/rahul/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -17,14 +21,31 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 
+    @Null
     private OffsetDateTime createDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 }
